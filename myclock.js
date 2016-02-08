49027f90765b8dcb7bc3a291dbb431d484328a4c
Ethan Ward <ethanandrewward@gmail.com>
2016-02-08 15:45:53 -0600

    Make clock function own file

--- myclock.js
+++ myclock.js
@@ -1,1 +1,13 @@
+function clock() {
 
+    var time = new Date();
+
+    var seconds = time.getSeconds();
+    var minutes = time.getMinutes();
+    var hours = time.getHours();
+
+    var currentTime = hours + ":" + minutes + ":" + seconds;
+
+    document.getElementById("Timer").firstChild.nodeValue = currentTime;
+
+}
