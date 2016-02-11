24f39094208dd13edfa307ae2cada4d689c5fad4
NeisesMike <neisesmichael@gmail.com>
2016-02-10 18:30:43 -0600

    Declare variables inside function

--- myclock.js
+++ myclock.js
@@ -1,15 +1,15 @@
 function clock() {
+	
+	var userTime = new Date(2016, 1, 15, 12, 33, 24, 567);
 
-	userTime = new Date(2016, 1, 15, 12, 33, 24, 567);
-
-	diffTime = new Date() - startTime;
+	var diffTime = new Date() - startTime;
 	userTime.setMilliseconds(userTime.getMilliseconds() + diffTime);
 
-	seconds = userTime.getSeconds();
-	minutes = userTime.getMinutes();
-	hours = userTime.getHours();
+	var seconds = userTime.getSeconds();
+	var minutes = userTime.getMinutes();
+	var hours = userTime.getHours();
 
-	currentTime = hours + ":" + minutes + ":" + seconds;
+	var currentTime = hours + ":" + minutes + ":" + seconds;
 
 	document.getElementById("Timer").firstChild.nodeValue = currentTime;
 
