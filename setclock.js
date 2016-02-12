64f3e5dadb38fcaf6166388d1200ca216cd33550
Travis Augustine <augustine@ku.edu>
2016-02-11 21:15:13 -0600

    Update set clock function to set the hours and minutes

--- setclock.js
+++ setclock.js
@@ -2,8 +2,26 @@
 //Last Edited: Feb 8
 
 function setclock(h, m, s) {
-    time = new Date();
-    time.setHours(h,m,s);
-    currentTime = hours + ":" + minutes + ":" + seconds;
-    document.getElementById("Timer").firstChild.nodeValue = currentTime;
+    //time = new Date();
+   // time.setHours(h,m,s);
+    //currentTime = hours + ":" + minutes + ":" + seconds;
+    //document.getElementById("Timer").firstChild.nodeValue = currentTime;
+    
+    
+    	{
+
+		userHours = hours;
+		userMinutes = minutes;
+
+		if( isNaN(userHours) )
+		{
+			userHours = 0;
+		}
+		
+		if( isNaN(userMinutes) )
+		{
+			userMinutes = 0;
+		}		
+
+	}
 }
