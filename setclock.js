f7c45dd5ec248f1666a2eb59780e8fd05d97cb7f
Travis Augustine <augustine@ku.edu>
2016-02-11 21:21:32 -0600

    Update function parameters

--- setclock.js
+++ setclock.js
@@ -1,7 +1,7 @@
 //Author: Michael Neises
 //Last Edited: Feb 8
 
-function setclock(h, m, s) {
+function setclock(hours, minutes) {
     //time = new Date();
    // time.setHours(h,m,s);
     //currentTime = hours + ":" + minutes + ":" + seconds;
